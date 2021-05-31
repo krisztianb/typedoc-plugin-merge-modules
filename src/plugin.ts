de0ecb0f44ec4985ebb2ec9239d718f82de694c2
beijaflor <sho.otani@gmail.com>
2021-05-31 16:55:55 +0900

    add merge mode option

--- src/plugin.ts
+++ src/plugin.ts
@@ -76,48 +76,51 @@ export class Plugin {
         const project = context.project;
         const modules = (project.children ?? []).filter((c) => c.kindOf(ReflectionKind.Module));
 
-        // conbine module DeclarationReflection by its module name
-        const combinedModules: Record<string, DeclarationReflection[]> = {};
-        modules.forEach((module) =>
-            combinedModules[module.name]
-                ? combinedModules[module.name].push(module)
-                : (combinedModules[module.name] = [module]),
-        );
+        switch (this.options.modeDefaults) {
+            case "project":
+                if (modules.length > 0) {
+                    project.children = [];
 
-        // reduce multiple DeclarationReflection into single declaration
-        for (const modName in combinedModules) {
-            const mods = combinedModules[modName];
-            const children = mods
-                .map((m) => m.children)
-                .reduce((acc, val) => (val ? (acc || []).concat(val) : acc), []);
-            // use first module as a principle module
-            mods[0].children = children;
-            // remove rest modules
-            for (let i = 1; i < mods.length; i++) {
-                mods[i].children = undefined;
-                project.removeReflection(mods[i]);
-            }
-        }
+                    for (const mod of modules) {
+                        const reflections = mod.children ?? [];
 
-        /*
-        if (modules.length > 0) {
-            project.children = [];
+                        for (const ref of reflections) {
+                            // Drop aliases
+                            if (!ref.kindOf(ReflectionKind.Reference)) {
+                                ref.parent = project;
+                                project.children.push(ref);
+                            }
+                        }
 
-            for (const mod of modules) {
-                const reflections = mod.children ?? [];
-
-                for (const ref of reflections) {
-                    // Drop aliases
-                    if (!ref.kindOf(ReflectionKind.Reference)) {
-                        ref.parent = project;
-                        project.children.push(ref);
+                        mod.children = undefined;
+                        project.removeReflection(mod);
                     }
                 }
+                break;
+            case "module":
+                // conbine module DeclarationReflection by its module name
+                const combinedModules: Record<string, DeclarationReflection[]> = {};
+                modules.forEach((module) =>
+                    combinedModules[module.name]
+                        ? combinedModules[module.name].push(module)
+                        : (combinedModules[module.name] = [module]),
+                );
 
-                mod.children = undefined;
-                project.removeReflection(mod);
-            }
+                // reduce multiple DeclarationReflection into single declaration
+                for (const modName in combinedModules) {
+                    const mods = combinedModules[modName];
+                    const children = mods
+                        .map((m) => m.children)
+                        .reduce((acc, val) => (val ? (acc || []).concat(val) : acc), []);
+                    // use first module as a principle module
+                    mods[0].children = children;
+                    // remove rest modules
+                    for (let i = 1; i < mods.length; i++) {
+                        mods[i].children = undefined;
+                        project.removeReflection(mods[i]);
+                    }
+                }
+                break;
         }
-        */
     }
 }
--- src/plugin_options.ts
+++ src/plugin_options.ts
@@ -1,5 +1,7 @@
 import { Application, ParameterType } from "typedoc";
 
+type Mode = "project" | "module";
+
 /**
  * Extend typedoc's options with the plugin's option using declaration merging.
  */
@@ -7,6 +9,7 @@ declare module "typedoc" {
     // eslint-disable-next-line @typescript-eslint/consistent-type-definitions -- This is not a separate type.
     export interface TypeDocOptionMap {
         mergeModulesRenameDefaults?: boolean;
+        mergeModulesMergeMode?: Mode;
     }
 }
 
@@ -17,12 +20,23 @@ export class PluginOptions {
     /** Defines if the plugin should rename default exports to their original name. */
     private _renameDefaults = true;
 
+    /** Defines if the plugin combine modules into project self or by module name. */
+    private _modeDefaults: Mode = "project";
+
     /**
      * Returns if the plugin should rename default exports to their original name.
      * @returns True, if the plugin should rename default exports to their original name, otherwise false.
      */
     public get renameDefaults(): boolean {
         return this._renameDefaults;
+    }
+
+    /**
+     * Returns if the plugin combine modules into project self or by module name.
+     * @returns 'project', if the plugin combine modules into project self or by module name, otherwise 'module'.
+     */
+    public get modeDefaults(): Mode {
+        return this._modeDefaults;
     }
 
     /**
@@ -37,6 +51,15 @@ export class PluginOptions {
             help: "If true default exports are renamed to their original name as their module is merged.",
             defaultValue: true,
         });
+
+        typedoc.options.addDeclaration({
+            type: ParameterType.String,
+            name: "mergeModulesMergeMode",
+            help:
+                // eslint-disable-next-line max-len
+                "Move all modules into project itself with 'project' ( default ). If 'module' set, modules combined by its name",
+            defaultValue: "project",
+        });
     }
 
     /**
@@ -45,5 +68,6 @@ export class PluginOptions {
      */
     public readValuesFromApplication(typedoc: Readonly<Application>): void {
         this._renameDefaults = typedoc.options.getValue("mergeModulesRenameDefaults") ?? this._renameDefaults;
+        this._modeDefaults = typedoc.options.getValue("mergeModulesMergeMode") ?? this._modeDefaults;
     }
 }
