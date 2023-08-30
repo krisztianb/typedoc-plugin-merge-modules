ee9436b83db62d2c867770d306f9c8e7037c01b6
Krisztian Balla <k.balla@gmx.at>
2023-08-30 21:37:50 +0200

    Fix "merge-module-category" tests

--- test/merge-module-category/test.cy.ts
+++ test/merge-module-category/test.cy.ts
@@ -4,9 +4,8 @@ describe("index.html", () => {
         cy.visit("./merge-module-category/output/index.html");
     });
 
-    it("contains a separate module and class link for the class E which has no @module jsdoc tag", () => {
+    it("contains a separate module link for the class E which has no @module jsdoc tag", () => {
         cy.get("nav").find("a[href='modules/e.html']");
-        cy.get("nav").find("a[href='classes/e.E.html']");
     });
 
     it("contains 3 merged module links", () => {
@@ -14,14 +13,45 @@ describe("index.html", () => {
         cy.get("nav").find("a[href='modules/merged-1.html']"); // for C
         cy.get("nav").find("a[href='modules/merged-2.html']"); // for D
     });
+});
 
-    it("merged the modules of A and B", () => {
-        cy.get("nav").find("a[href='classes/merged.A.html']");
-        cy.get("nav").find("a[href='classes/merged.B.html']");
+describe("modules/e.html", () => {
+    beforeEach(() => {
+        cy.visit("./merge-module-category/output/modules/e.html");
     });
 
-    it("didn't merge the modules of C and D", () => {
-        cy.get("nav").find("a[href='classes/merged-1.C.html']");
-        cy.get("nav").find("a[href='classes/merged-2.D.html']");
+    it("contains a sub link to the class E", () => {
+        cy.get("nav").find("a[href='../classes/e.E.html']");
+    });
+});
+
+describe("modules/merged.html", () => {
+    beforeEach(() => {
+        cy.visit("./merge-module-category/output/modules/merged.html");
+    });
+
+    it("contains sub links to the classes A and B", () => {
+        cy.get("nav").find("a[href='../classes/merged.A.html']");
+        cy.get("nav").find("a[href='../classes/merged.B.html']");
+    });
+});
+
+describe("modules/merged-1.html", () => {
+    beforeEach(() => {
+        cy.visit("./merge-module-category/output/modules/merged-1.html");
+    });
+
+    it("contains a sub link to the class C", () => {
+        cy.get("nav").find("a[href='../classes/merged-1.C.html']");
+    });
+});
+
+describe("modules/merged-2.html", () => {
+    beforeEach(() => {
+        cy.visit("./merge-module-category/output/modules/merged-2.html");
+    });
+
+    it("contains a sub link to the class D", () => {
+        cy.get("nav").find("a[href='../classes/merged-2.D.html']");
     });
 });
