8886c4c0c5133a3c8b9ea976ef30c76006ccb797
Krisztian Balla <k.balla@gmx.at>
2024-09-21 16:51:11 +0200

    Adjust test cases default-export-rename-on and default-export-rename-off to use a typedoc.json config file

--- test/test.js
+++ test/test.js
@@ -4,33 +4,27 @@ const { execSync } = require("child_process");
 const execOptions = { stdio: "inherit" };
 
 console.log("===================================== TEST MERGE OFF ===============================================");
-// prettier-ignore
 execSync("cd merge-off && npx typedoc", execOptions);
 execSync("call npx cypress run --quiet --spec 'merge-off/test.cy.ts'", execOptions);
 
 console.log("=================================== TEST MERGE PROJECT =============================================");
-// prettier-ignore
 execSync("cd merge-project && npx typedoc", execOptions);
 execSync("npx cypress run --quiet --spec 'merge-project/test.cy.ts'", execOptions);
 
 console.log("==================================== TEST MERGE MODULE =============================================");
-// prettier-ignore
 execSync("cd merge-module && npx typedoc", execOptions);
 execSync("npx cypress run --quiet --spec 'merge-module/test.cy.ts'", execOptions);
 
 console.log("================================ TEST MERGE MODULE CATEGORY ========================================");
-// prettier-ignore
 execSync("cd merge-module-category && npx typedoc", execOptions);
 execSync("npx cypress run --quiet --spec 'merge-module-category/test.cy.ts'", execOptions);
 
 console.log("============================== TEST DEFAULT EXPORT RENAMING ON =====================================");
-// prettier-ignore
-execSync("npx typedoc --tsconfig default-export-rename-on/tsconfig.json --plugin typedoc-plugin-merge-modules --mergeModulesRenameDefaults true --entryPointStrategy expand --out default-export-rename-on/output default-export-rename-on/input", execOptions);
+execSync("cd default-export-rename-on && npx typedoc", execOptions);
 execSync("npx cypress run --quiet --spec 'default-export-rename-on/test.cy.ts'", execOptions);
 
 console.log("============================== TEST DEFAULT EXPORT RENAMING OFF ====================================");
-// prettier-ignore
-execSync("npx typedoc --tsconfig default-export-rename-off/tsconfig.json --plugin typedoc-plugin-merge-modules --mergeModulesRenameDefaults false --entryPointStrategy expand --out default-export-rename-off/output default-export-rename-off/input", execOptions);
+execSync("cd default-export-rename-off && npx typedoc", execOptions);
 execSync("npx cypress run --quiet --spec 'default-export-rename-off/test.cy.ts'", execOptions);
 
 console.log("=============================== TEST MERGE PROJECT ON MONOREPO =====================================");
